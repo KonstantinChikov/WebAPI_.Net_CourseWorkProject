86ec2347e6fba5a8162ffff9bb08525e55ebd949
Konstantin Chikov <91802179+KonstantinChikov@users.noreply.github.com>
2022-11-21 00:03:13 +0200

    Add files via upload

---
+++ APICourseWorkProject/APICourseWorkProject.API/Controllers/AuthController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+
+namespace APICourseWorkProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        // TO DO : put all of this shit in a servise, get it out of the controller
+
+        public static User user = new User();   // I have only ONE user for now
+
+
+
+        private readonly IConfiguration _configuration;
+        public AuthController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+
+        //User Register and Login Methods
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.Username = request.UserName;
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            return Ok(user);
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(UserDto request)
+        {
+            // check if Username is here already
+            if (user.Username == request.UserName)
+            {
+                return BadRequest(" User NOT found ");
+            }
+            // check password
+            if(!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                return BadRequest(" Wrong password ");
+            }
+
+            //json web token
+            string token = CreateToken(user);
+
+            return Ok(token);
+
+        }
+
+
+
+        //json web token Method
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+
+
+        //Password Create and Verify methods
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt =  hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));  
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);    //comparing the computedHash value byte by byte with the passwordHash
+            }    
+        }
+
+
+
+
+
+    }
+}
---
+++ APICourseWorkProject/APICourseWorkProject.API/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace APICourseWorkProject.API
+{
+    public class UserDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
